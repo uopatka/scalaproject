4a5bfb56ceb7c1bb5f92c9a56c4acb4a38c44706
Julia Masiarz <julciax18@gmail.com>
2025-12-29 14:39:07 +0100

    Editing the cover - update

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -106,7 +106,7 @@ class HomeController @Inject()(
     val maybeUserId: Option[Long] =
       request.session.get("userId").map(_.toLong)
 
-    val userId: Long = maybeUserId.getOrElse(1L) // guest = 0
+    val userId: Long = maybeUserId.getOrElse(0L) // guest = 0
 
     bookForm.bindFromRequest().fold(
       formWithErrors =>
@@ -129,7 +129,7 @@ class HomeController @Inject()(
           } else {
             openLibraryService.fetchByIsbn(isbn).flatMap {
               case Some(fetchedBook) =>
-                val ensureGuestF: Future[Unit] = if (userId == 1L) {
+                val ensureGuestF: Future[Unit] = if (userId == 0L) {
                   userRepository.getById(0L).flatMap {
                     case Some(_) => Future.successful(())
                     case None    => userRepository.insert(User(0L, "guest", "")).map(_ => ())
@@ -241,20 +241,24 @@ class HomeController @Inject()(
       }
     )
   }
+
   def editBookCover(entryId: Long) = Action(parse.multipartFormData).async { implicit request =>
     request.body.file("cover") match {
       case Some(coverFile) =>
         val filename = java.time.Instant.now().toEpochMilli + "_" + coverFile.filename
         val filePath = s"public/uploads/$filename"
-        coverFile.ref.moveTo(new java.io.File(filePath), replace = true)
 
-        // Pobierz entry z repozytorium
+        coverFile.ref.moveTo(new java.io.File(filePath),
+          replace = true)
+
+        // Getting the entry id
         bookEntryRepository.getById(entryId).flatMap {
           case Some(entry) =>
-            // Pobierz książkę po ISBN
+            // Get book's ISBN
             bookRepository.getByIsbn(entry.isbn).flatMap {
               case Some(book) =>
                 val updatedBook = book.copy(cover = s"/assets/uploads/$filename")
+
                 bookRepository.update(updatedBook).map(_ =>
                   Redirect(routes.HomeController.editBookEntry(entryId))
                 )

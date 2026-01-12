db52312de32766c3a9025c4721c4ca2feac18646
uopatka <ola.woznica@gmail.com>
2026-01-12 22:53:46 +0100

    Style changes

--- app/models/BookStatus.scala
+++ app/models/BookStatus.scala
@@ -14,4 +14,11 @@ object BookStatus {
 
   def fromString(s: String): Option[BookStatus] =
     all.find(_.value == s)
+
+  def label(status: BookStatus): String = status match {
+    case ToRead   => "Do przeczytania"
+    case Reading  => "W trakcie"
+    case Finished => "Przeczytane"
+    case Dropped  => "Porzucone"
+  }
 }

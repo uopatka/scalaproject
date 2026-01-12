e660a64e4d537999eb49558689b6222e8c83f52d
uopatka <ola.woznica@gmail.com>
2026-01-12 21:03:34 +0100

    Fixed page calculation for publications

--- app/models/Publication.scala
+++ app/models/Publication.scala
@@ -4,9 +4,15 @@ final case class Publication(doi: String, title: String,
                             authors: String, publishYear: Int = 0,
                             pages: String, cover: String = "") extends DisplayItem {
   def pageCount: Int = {
-    val pattern = """.*\((\d+) pages\).*""".r
-    pages match {
-      case pattern(count) => count.toInt
+    if (pages == null || pages.isEmpty) return 0
+
+    pages.split("-") match {
+      case Array(startStr, endStr) =>
+        (startStr.replaceAll("[^0-9]", ""), endStr.replaceAll("[^0-9]", "")) match {
+          case (start, end) if start.nonEmpty && end.nonEmpty =>
+            end.toInt - start.toInt + 1
+          case _ => 0
+        }
       case _ => 0
     }
   }
